51c54e71b9a252d86a4a82a07008d4a2a1342d68
mii0829 <kanokanosakusaku@gmail.com>
2023-03-19 16:50:31 +0900

    finish

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
 $(function($) {
 
+
+
+
     $(window).scroll(function () {
-        var targetElement = $('.fadein').offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        if (scroll > targetElement - windowHeight + 200) {
-            $('.fadein').css('opacity', '1');
-            $('.fadein').css('transform','translateY(0)');
-        }
-    });
+        $(".effect-fade").each(function () {
+          var elemPos = $(this).offset().top; 
+          var scroll = $(window).scrollTop(); 
+          var windowHeight = $(window).height(); 
+          if (scroll > elemPos - windowHeight) {
+            $(this).addClass("effect-scroll");
+          }
+        });
+      });
+
+
+
+    $('nav a[href^="#"]').click(function() {
+        var adjust = 50;
+        var speed = 1000;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - adjust;
+        $('body,html').animate( {
+            scrollTop: position
+        }, speed, 'swing');
+        return false;
+    })
+
+  
+
 
     particlesJS("particles-js", {
         "particles":{"number":{"value":19,"density":{"enable":true,"value_area":2051.7838682439087}},
@@ -20,5 +41,9 @@ $(function($) {
         "move":{"enable":true, "speed":5.620472365193136, "direction":"top", "random":false, "straight":false, "out_mode":"out", "bounce":false, "attract":{"enable":false,"rotateX":600,"rotateY":1200}}},
         "interactivity":{"detect_on":"canvas",
         "events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"repulse"},"resize":true},
-        "modes":{"grab":{"distance":400,"line_linked":{"opacity":0.5}},"bubble":{"distance":400,"size":31.96803196803197,"duration":0.3,"opacity":1,"speed":3},"repulse":{"distance":127.87212787212788,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+        "modes":{"grab":{"distance":400,"line_linked":{"opacity":0.5}},"bubble":{"distance":400,"size":31.96803196803197,"duration":0.3,"opacity":1,"speed":3},"repulse":{"distance":127.87212787212788,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true
+    });
+    
+    var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); 
+        if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
 });
